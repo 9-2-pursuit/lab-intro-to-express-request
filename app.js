f8c4a1def50343f44183f67845759665ad31fc81
hector <52504713+hector918@users.noreply.github.com>
2022-12-15 20:44:36 +0000

    fixed some bugs

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ require('dotenv').config();
 const pokemon = require("./models/pokemon.json");
 const PORT = process.env.PORT;
 const app = express();
-const basic_css = "<style>h1{color:red}body{backgroundcolor:gray}a{cursor: pointer;}</style>";
+const basic_css = "<style>h1{color:red}body{backgroundcolor:gray}a{cursor: pointer;}div{border: 2px dotted greenyellow;box-shadow: 5px 5px cadetblue;margin: 10px;padding: 20px;}li{margin: 5px;padding: 10px;}</style>";
+
 
 app.get("/", (req, res) => {
   //
   res.send("Welcome 99 Pokemon");
 });
+
 app.get("/bugs", (req, res) => {
   res.send(`${basic_css}<h1>99 little bugs in the code</h1><a href='/bugs/101'>Pull one down, patch it around</a>`);
 });
+
 app.get("/bugs/:numberOfBugs", (req, res) => {
   let res_text = "";
 
@@ -26,7 +29,7 @@ app.get("/bugs/:numberOfBugs", (req, res) => {
   res.send(res_text);
 });
 
-app.get("/pokemon",( q,s ) =>{
+app.get("/pokemon",( q, s ) =>{
   s.send(pokemon);
 })
 
@@ -35,6 +38,8 @@ app.get("/pokemon/search",( q, s ) =>{
     let _ret = undefined;
     for(let x in q.query)
     {
+
+      /////////
       if(el[x]!=undefined && _ret===undefined && (el[x].toLowerCase() === q.query[x].toLowerCase()))
       {
         _ret = true;
@@ -53,7 +58,7 @@ app.get("/pokemon/search",( q, s ) =>{
   s.send([]);
 })
 
-app.get("/pokemon/:indexOfArray",( q,s ) =>{
+app.get("/pokemon/:indexOfArray",( q, s ) =>{
   const result = pokemon[q.params.indexOfArray];
   if(result)
   {
@@ -66,13 +71,49 @@ app.get("/pokemon/:indexOfArray",( q,s ) =>{
   
 })
 
-app.get("/run/runny/runner",( q,s ) =>{
-  s.send("Congratulations on starting a new project called run-runny-runner!");
+app.get("/:verb/:adjective/:noun",( q, s ) =>{
+  const { verb, adjective, noun } = q.params;
+  s.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
 })
 
+app.get("/pokemon-pretty",( q, s ) =>{
+  s.send(`${basic_css}${pokemon.map(el=>`<div>${recurtive_c(el)}</div>`).join("")}`);
+  // s.send(createHTML_json(recurtive_c(pokemon),null));
+})
 
 app.listen(PORT, ()=> {
   console.log(`listening on port ${PORT}`)
 })
 
 module.exports = app;
+
+function recurtive_c(json)
+{
+  let ret = "";
+  for(let x in json)
+  {
+    if(typeof json[x]==="object")
+    {
+      ret+=`<h4>${x}</h4><li>${(recurtive_c(json[x]))}</li>`;
+    }
+    else
+    {
+      if(Array.isArray(json))
+      {
+        ret+=`<p><span>${json[x]}</span></p>`;
+      }
+      else
+      {
+        switch(x)
+        {
+          case "img":
+            ret+=`<img src="${json[x]}"/>`;
+          break;
+          default:
+            ret+=`<p><strong>${x}:</strong> <span>${json[x]}</span></p>`;
+        }
+      } 
+    }
+  }
+  return ret;
+}
