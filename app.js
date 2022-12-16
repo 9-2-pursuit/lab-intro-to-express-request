c3d778c36d8939bdd03badc7c8dc8022a9512f4e
i-guerrero <isaacguerrero@pursuit.org>
2022-12-16 16:02:29 -0500

    Add new project name generator

--- app.js
+++ app.js
@@ -9,5 +9,13 @@ app.get("/", (req, res) => {
   res.send("Welcome 99 Pokemon");
 });
 
+// New Project Name Generator
+app.get("/:verb/:adjective/:noun", (req, res) => {
+  const { verb, adjective, noun } = req.params;
+  res.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
+  );
+});
+
 // EXPORT
 module.exports = app;
